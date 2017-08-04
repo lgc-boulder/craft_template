7f81e1684850cffb31b47dec616f4016e0cbf304
Luke Griffiths <wlgriffiths@gmail.com>
2017-08-04 14:55:44 -0600

    [refc] move component to components directory

--- src/components/App.js
+++ src/components/App.js
@@ -1,8 +1,9 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
 
 import { Button } from 'reactstrap'
 import Icon from 'react-fontawesome'
+
+import MessageContainer from '../containers/MessageContainer'
 
 class App extends Component {
   render() {
@@ -13,15 +14,23 @@ class App extends Component {
         <div className='jumbotron'>
           <h1> LGC Boulder App Template </h1>
 
+          <hr/>
+
           <ul className='text-info'>
             <li>bootstrap is available via reactstrap</li>
             <li>font-awesome is available via react-fontawesome</li>
           </ul>
 
+          <hr/>
+
           <Button>
             <Icon name='eye' />
             This is a reactstrap button
           </Button>
+
+          <hr/>
+
+          <MessageContainer />
         </div>
       </div>
     );
